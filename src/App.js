3780f0d8f517a712c7b492ed939b815a310e7fda
EJShim <soulrommel@gmail.com>
2019-10-04 16:37:12 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Module } from 'module';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount(){
+    console.log(this.refs.renderwindow)
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <canvas className="App-renderwindow" ref="renderwindow"/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
